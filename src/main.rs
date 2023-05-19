900b674be5c9887c4ddf95266f1c63b6f801d79d
Guzman Zugnoni <gooseiman@protonmail.com>
2023-05-19 15:26:44 -0300

    parse attributes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Context;
 use quick_xml::{
-    events::{BytesDecl, Event},
+    events::{attributes::{Attribute, Attributes}, BytesDecl, BytesStart, Event},
+    name::QName,
     reader::Reader,
 };
 use std::fs::File;
@@ -25,8 +26,55 @@ struct EV3Project {
 
 struct EV3File {
     decl: BytesDecl<'static>,
+    version: String,
     name: String,
     contents: Vec<u8>,
+}
+
+struct ParsedAttribute {
+    key: (String, Option<String>),
+    value: String,
+}
+
+impl ParsedAttribute {
+    fn parse(attr: &Attribute) -> anyhow::Result<ParsedAttribute> {
+        let (name, prefix) = Self::extract_name_from_qname(attr.key)?;
+        let value = String::from_utf8(attr.value.clone().into_owned())
+            .context(format!("Invalid UTF-8 in {name} tag value"))?;
+        Ok(ParsedAttribute {
+            key: (name, prefix),
+            value,
+        })
+    }
+
+    fn extract_name_from_qname(qname: QName) -> anyhow::Result<(String, Option<String>)> {
+        let (name, prefix) = qname.decompose();
+        let name = String::from_utf8(name.into_inner().to_vec()).context("Invalid UTF-8 in tag")?;
+        let prefix = prefix
+            .map(|s| String::from_utf8(s.into_inner().to_vec()))
+            .transpose()
+            .context("Invalid UTF-8 in tag prefix")?;
+        Ok((name, prefix))
+    }
+}
+
+trait HasAttribute {
+    fn attributes(&self) -> Attributes;
+}
+
+impl HasAttribute for BytesStart<'_> {
+    fn attributes(&self) -> Attributes {
+        self.attributes()
+    }
+}
+
+fn parse_attributes(tag: &impl HasAttribute) -> anyhow::Result<Vec<ParsedAttribute>> {
+    let mut v = vec![];
+    for attr in tag.attributes() {
+        let attr = attr.context("Malformed attribute")?;
+        v.push(ParsedAttribute::parse(&attr).context("Failed parsing attribute")?);
+    }
+    Ok(v)
 }
 
 impl EV3File {
@@ -37,13 +85,26 @@ impl EV3File {
         loop {
             match xml.read_event().context("File had invalid xml")? {
                 Event::Start(t) => {
-                    println!("Start tag: name {}", str::from_utf8(t.name().0).unwrap())
+                    let (name, prefix) = t.name().decompose();
+                    let name = String::from_utf8(name.into_inner().iter().cloned().collect())
+                        .context(format!("Invalid UTF-8 tag name {:?}", name))?;
+                    if let Some(_) = prefix {
+                        println!("TODO: Start with prefix!");
+                    } else {
+                        match name.as_str() {
+                            "SourceFile" => {
+                                let attributes = parse_attributes(&t).context("Failed parsing attributes")?;
+                                for attribute in attributes {
+                                    println!("Attr: name({}), value({})", attribute.key.0, attribute.value);
+                                }
+                            }
+                            _ => println!("TODO: start tag {name}"),
+                        }
+                    }
                 }
-                Event::End(t) => println!("End tag: name {}", str::from_utf8(t.name().0).unwrap()),
-                Event::Empty(t) => {
-                    println!("Empty tag: name {}", str::from_utf8(t.name().0).unwrap())
-                }
-                Event::Text(_) => println!("Found Text"),
+                Event::End(_) => println!("TODO: End tag"),
+                Event::Empty(t) => println!("TODO: Empty tag"),
+                Event::Text(_) => println!("TODO: Text tag"),
                 Event::Comment(_) => println!("Ignoring Comment"),
                 Event::CData(_) => println!("Found CData"),
                 Event::Decl(d) => decl = Some(d),
