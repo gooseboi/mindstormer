bb8c9acc306ff3a087005b96b78b69c5a85602d1
Guzman Zugnoni <gooseiman@protonmail.com>
2023-06-14 12:21:11 -0300

    move every source file to examples dir

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod utils;
 use ev3::project::EV3Project;
 
 fn main() -> anyhow::Result<()> {
-    let project = EV3Project::get_project_from_zip("1block.ev3")?;
+    let project = EV3Project::get_project_from_zip("examples/1block.ev3")?;
     project.output_file("out.ev3")?;
     Ok(())
 }
